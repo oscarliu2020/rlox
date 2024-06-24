5f1feea93f261b5a9162f1575a268f5e167a7a35
Oscar Liu <25657114+oscarliu2020@users.noreply.github.com>
2024-06-24 14:12:42 +0800

    modified bench1.rs

--- benches/bench1.rs
+++ benches/bench1.rs
@@ -1,9 +1,13 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use rlox::runner::run_file;
 fn criterion_benchmark(c: &mut Criterion) {
-    let mut group=c.benchmark_group("benches");
-    group.sample_size(10);
-    group.bench_function("bench1", |b| b.iter(|| run_file(black_box("test_data/benches/fib.lox"))));
+    let mut group = c.benchmark_group("benches");
+    group
+        .sample_size(10)
+        .measurement_time(std::time::Duration::from_secs(40));
+    group.bench_function("bench1", |b| {
+        b.iter(|| run_file(black_box("test_data/benches/fib.lox")))
+    });
     group.finish();
 }
 criterion_group!(benches, criterion_benchmark);
